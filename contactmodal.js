33fe7cc98624f95b61d52f23471900e3297d3643
modzel1990 <35817812+modzel1990@users.noreply.github.com>
2018-02-08 09:58:25 +0000

    Create contactmodal.js

---
+++ contactmodal.js
@@ -0,0 +1,22 @@
+//it's not my code, borrowed and included in to my work
+//also it does not work correctly on my project yet so I need to change it a bit
+
+
+
+$(function() {
+
+  // contact form animations
+  $('#contact').click(function() {
+    $('#contactForm').fadeToggle();
+  })
+  $(document).mouseup(function (e) {
+    var container = $("#contactForm");
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.fadeOut();
+    }
+  });
+
+});
