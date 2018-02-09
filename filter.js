c3a4677a1e267f5e3d88676bf81735bcb46b6553
Rafal Modzelewski <35817812+modzel1990@users.noreply.github.com>
2018-02-09 11:26:59 +0000

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,3 +1,7 @@
+
+// for some reason 'toLowerCase' does not work, so in search bar the input must match exactly what is in the data-filter
+// still working on it
+
 var divs = $('div[data-filter]');
 $('#search').on('keyup', function() {
     var val = $.trim(this.value).toLowerCase(this.value);
