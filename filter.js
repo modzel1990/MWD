dbbab1bd7c0600cb41dfc33f69c4dccbcb32bc13
Rafal Modzelewski <35817812+modzel1990@users.noreply.github.com>
2018-02-12 10:02:53 +0000

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,12 +1,15 @@
+var divs = $("div[data-filter]");
 
-// for some reason 'toLowerCase' does not work, so in search bar the input must match exactly what is in the data-filter
-// still working on it
-
-var divs = $('div[data-filter]');
 $('#search').on('keyup', function() {
-    var val = $.trim(this.value).toLowerCase(this.value);
-    divs.hide();
-    divs.filter(function() {
-        return $(this.data('filter').toLowerCase().search(val) >= 0)
-    }).show();
+	var filter = $(this).val().toLowerCase();
+	filterList(filter);
 });
+
+//filter function
+function filterList(value) {
+  $(divs).fadeOut("fast");
+  divs.filter(function() {
+  return $(this).attr("data-filter").toLowerCase().indexOf(value) >= 0
+}).fadeIn("fast")
+};
+
