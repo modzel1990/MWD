591ea476877ebf672c24e13822ec8c8891267f7c
Rafal Modzelewski <35817812+modzel1990@users.noreply.github.com>
2018-02-23 11:25:08 +0000

    Add files via upload

--- properties.blade.php
+++ properties.blade.php
@@ -10,86 +10,7 @@
         <!--************ HERO ***************************************************************************************-->
         <!--*********************************************************************************************************-->
         <header id="hero">
-
-            <nav class="navbar navbar-expand-lg navbar-light fixed-top">
-                <a class="navbar-brand" href="#page-top">
-                <h4 style="padding-top: 10px;"> Foot Forward Properties</h4>
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav ml-auto">
-                        <a class="nav-item nav-link active" href="/#page-top">Home <span class="sr-only">(current)</span></a>
-                        <a class="nav-item nav-link" href="/blog">Blog</a>
-                        <a class="nav-item nav-link" href="#properties">Properties</a>
-                        <a class="nav-item nav-link" href="/about">About</a>
-                        <a class="nav-item nav-link" href="/contact">Contact</a>
-                        <span class="divider"></span>
-                    </div>
-                    <div class="udpad">
-                      <i class="fas fa-phone-square"><span class="nav-info">01302 000000</span></i>
-                      <i class="fas fa-envelope-square"><span class="nav-info">example@email.com</span></i>
-                    </div>
-                </div>
-            </nav>
-
-            <div class="hero__background">
-                <div class="background-wrapper" data-parallax="scroll" data-parallax-speed="3">
-                    <div class="background">
-                        <div class="owl-carousel hero__slider parallax-element" data-owl-items="1" data-owl-autoplay="1" data-owl-dots="0" data-owl-nav="1" data-owl-loop="1" data-owl-fadeout="1" data-owl-nav-container=".slider-nav">
-                            <div class="slide img-into-bg">
-                                <img src="assets/img/slide-01.jpg" alt="">
-                            </div>
-                            <div class="slide img-into-bg">
-                                <img src="assets/img/slide-02.jpg" alt="">
-                            </div>
-                            <div class="slide img-into-bg">
-                                <img src="assets/img/slide-03.jpg" alt="">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!--end background-->
-            </div>
-
-            <div class="container">
-                <div class="row">
-                    <div class="hero__outer-wrapper">
-                        <div class="hero__inner-wrapper align-bottom">
-                            <div class="col-xl-5 col-lg-5 col-md-7">
-                                <div class="hero__caption has-dark-background">
-                                    <h1>Footforward Properties</h1>
-                                    <form class="form">
-                                        <div class="form-group">
-                                            <label for="form-hero-email">Subscribe to our newsletter for updates.</label>
-                                            <div class="input-group">
-                                                <input id="form-hero-email" type="text" class="form-control" placeholder="Your Email" aria-label="Your Email">
-                                                <span class="input-group-btn">
-                                                <button class="btn btn-secondary" type="button">
-                                                    <i class="fa fa-arrow-right"></i>
-                                                </button>
-                                            </span>
-                                            </div>
-
-                                        </div>
-                                        <!--end form-group -->
-                                    </form>
-                                    <hr>
-                                    <p>
-                                      We are a high quality, high standard, HMO company based in Doncaster with properties located all across South Yorkshire.
-                                    </p>
-                                    <a href="#properties" class="btn btn-primary btn-framed">Browse our Properties</a>
-                                </div>
-                            </div>
-                            <div class="slider-nav"></div>
-                        </div>
-                        <!--end container-->
-                    </div>
-                    <!--end hero__inner-wrapper-->
-                </div>
-            </div>
-            <!--end hero__outer-wrapper-->
+            @include('partials.nav')
         </header>
         <!--end hero-->
 
@@ -397,22 +318,22 @@
                                <div class="col-md-8">
                                  <div class="mod1 fade" class="mySlides">
                                    <div class="numbertext">1 / 4</div>
-                                   <img src="http://www.aptmags.com/wp-content/uploads/2014/01/2014-Modern-Apartment-Design-Living-Room-and-Kitchen-Set.jpg" style="width:100%; max-height: 60vh;"></br>
+                                   <img class="respsize" src="http://www.aptmags.com/wp-content/uploads/2014/01/2014-Modern-Apartment-Design-Living-Room-and-Kitchen-Set.jpg" style="width:100%; max-height: 60vh;"></br>
                                  </div>
 
                                  <div class="mod1 fade" class="mySlides">
                                    <div class="numbertext">2 / 4</div>
-                                   <img src="https://i.pinimg.com/736x/1d/bd/32/1dbd321aa618d0276d0a5263dab713d1--oscar-terrace.jpg" style="width:100%; max-height: 60vh;"></br>
+                                   <img class="respsize" src="https://i.pinimg.com/736x/1d/bd/32/1dbd321aa618d0276d0a5263dab713d1--oscar-terrace.jpg" style="width:100%; max-height: 60vh;"></br>
                                  </div>
 
                                  <div class="mod1 fade" class="mySlides">
                                    <div class="numbertext">3 / 4</div>
-                                   <img src="https://newbuyadvice.files.wordpress.com/2012/10/a-typical-interior-of-a-waterside-grange-property.jpg" style="width:100%; max-height: 60vh;"></br>
+                                   <img class="respsize" src="https://newbuyadvice.files.wordpress.com/2012/10/a-typical-interior-of-a-waterside-grange-property.jpg" style="width:100%; max-height: 60vh;"></br>
                                  </div>
 
                                  <div class="mod1 fade" class="mySlides">
                                    <div class="numbertext">4 / 4</div>
-                                   <img src="http://www.moko3d.com/files/Projects/Visualisation%20of%20sea%20view%20property%20in%20Brighton%20Arundel/Period%20details%20like%20ceiling%20mouldings%20in%20this%20visualisation.jpg" style="width:100%; max-height: 60vh;"></br>
+                                   <img class="respsize" src="http://www.moko3d.com/files/Projects/Visualisation%20of%20sea%20view%20property%20in%20Brighton%20Arundel/Period%20details%20like%20ceiling%20mouldings%20in%20this%20visualisation.jpg" style="width:100%; max-height: 60vh;"></br>
                                  </div>
 
                                  <!-- Caption text -->
@@ -524,12 +445,12 @@
                                <div class="col-md-8">
                                  <div class="mod2 fade" class="mySlides">
                                    <div class="numbertext">1 / 2</div>
-                                   <img src="http://cdclifestyle.com/wp-content/uploads/2011/10/luxury-property-show-home-interior.jpg" style="width:100%; max-height: 60vh;">
+                                   <img class="respsize" src="http://cdclifestyle.com/wp-content/uploads/2011/10/luxury-property-show-home-interior.jpg" style="width:100%; max-height: 60vh;">
                                  </div>
 
                                  <div class="mod2 fade" class="mySlides">
                                    <div class="numbertext">2 / 2</div>
-                                   <img src="http://vault.starproperty.my.s3.amazonaws.com/wp-content/uploads/2015/08/nu_4.jpg" style="width:100%; max-height: 60vh;">
+                                   <img class="respsize" src="http://vault.starproperty.my.s3.amazonaws.com/wp-content/uploads/2015/08/nu_4.jpg" style="width:100%; max-height: 60vh;">
                                  </div>
                                  <!-- Caption text -->
                                  <div class="row nopad">
@@ -618,17 +539,17 @@
                                <div class="col-md-8">
                                  <div class="mod3 fade" class="mySlides">
                                    <div class="numbertext">1 / 3</div>
-                                   <img src="http://stoneintl.net/wp-content/uploads/2017/05/The-Best-Kitchen-Cabinets.jpg" style="width:100%; max-height: 60vh;">
+                                   <img class="respsize" src="http://stoneintl.net/wp-content/uploads/2017/05/The-Best-Kitchen-Cabinets.jpg" style="width:100%; max-height: 60vh;">
                                  </div>
 
                                  <div class="mod3 fade" class="mySlides">
                                    <div class="numbertext">2 / 3</div>
-                                   <img src="https://mr3.homeflow-assets.co.uk/files/site_asset/image/3475/6318/_x_/PUT170080_05.jpg" style="width:100%; max-height: 60vh;">
+                                   <img class="respsize" src="https://mr3.homeflow-assets.co.uk/files/site_asset/image/3475/6318/_x_/PUT170080_05.jpg" style="width:100%; max-height: 60vh;">
                                  </div>
 
                                  <div class="mod3 fade" class="mySlides">
                                    <div class="numbertext">3 / 3</div>
-                                   <img src="https://i.pinimg.com/originals/47/22/a5/4722a5e8b1f1c10f7887e29a0f6b93eb.jpg" style="width:100%; max-height: 60vh;">
+                                   <img class="respsize" src="https://i.pinimg.com/originals/47/22/a5/4722a5e8b1f1c10f7887e29a0f6b93eb.jpg" style="width:100%; max-height: 60vh;">
                                  </div>
                                  <!-- Caption text -->
                                  <div class="row nopad">
@@ -721,11 +642,11 @@
                                <div class="col-md-8">
                                  <div class="mod4 fade" class="mySlides">
                                    <div class="numbertext">1 / 2</div>
-                                   <img src="http://www.suncityvillas.com/server13-cdn/2015/10/09/bathroom-ceiling-design-room-ceiling-design-99c38376887f449b.jpg" style="width:100%; max-height: 60vh;">
+                                   <img class="respsize" src="http://www.suncityvillas.com/server13-cdn/2015/10/09/bathroom-ceiling-design-room-ceiling-design-99c38376887f449b.jpg" style="width:100%; max-height: 60vh;">
                                  </div>
 
                                  <div class="mod4 fade" class="mySlides">
-                                   <div class="numbertext">2 / 2</div>
+                                   <div class="respsize" class="numbertext">2 / 2</div>
                                    <img src="https://content.makaan.com/16/3715550/274/9812848.jpeg?width=665&height=415" style="width:100%; max-height: 60vh;">
                                  </div>
                                  <!-- Caption text -->
@@ -815,12 +736,12 @@
                                <div class="col-md-8">
                                  <div class="mod5 fade" class="mySlides">
                                    <div class="numbertext">1 / 2</div>
-                                   <img src="http://www.suncityvillas.com/server13-cdn/2015/10/09/bathroom-ceiling-design-room-ceiling-design-99c38376887f449b.jpg" style="width:100%; max-height: 60vh;">
+                                   <img class="respsize" src="http://www.suncityvillas.com/server13-cdn/2015/10/09/bathroom-ceiling-design-room-ceiling-design-99c38376887f449b.jpg" style="width:100%; max-height: 60vh;">
                                  </div>
 
                                  <div class="mod5 fade" class="mySlides">
                                    <div class="numbertext">2 / 2</div>
-                                   <img src="https://content.makaan.com/16/3715550/274/9812848.jpeg?width=665&height=415" style="width:100%; max-height: 60vh;">
+                                   <img class="respsize" src="https://content.makaan.com/16/3715550/274/9812848.jpeg?width=665&height=415" style="width:100%; max-height: 60vh;">
                                  </div>
                                  <!-- Caption text -->
                                  <div class="row nopad">
@@ -909,12 +830,12 @@
                                <div class="col-md-8">
                                  <div class="mod6 fade" class="mySlides">
                                    <div class="numbertext">1 / 2</div>
-                                   <img src="http://www.suncityvillas.com/server13-cdn/2015/10/09/bathroom-ceiling-design-room-ceiling-design-99c38376887f449b.jpg" style="width:100%; max-height: 60vh;">
+                                   <img class="respsize" src="http://www.suncityvillas.com/server13-cdn/2015/10/09/bathroom-ceiling-design-room-ceiling-design-99c38376887f449b.jpg" style="width:100%; max-height: 60vh;">
                                  </div>
 
                                  <div class="mod6 fade" class="mySlides">
                                    <div class="numbertext">2 / 2</div>
-                                   <img src="https://content.makaan.com/16/3715550/274/9812848.jpeg?width=665&height=415" style="width:100%; max-height: 60vh;">
+                                   <img class="respsize" src="https://content.makaan.com/16/3715550/274/9812848.jpeg?width=665&height=415" style="width:100%; max-height: 60vh;">
                                  </div>
                                  <!-- Caption text -->
                                  <div class="row nopad">
